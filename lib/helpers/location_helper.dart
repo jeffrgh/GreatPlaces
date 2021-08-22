eaf560e32018f2f85041dc73c93e813fed42ed3f
jeffrgh <84900057+jeffrgh@users.noreply.github.com>
2021-08-22 11:58:42 +0530

    Add files via upload

---
+++ lib/helpers/location_helper.dart
@@ -0,0 +1,8 @@
+const GOOGLE_API_KEY = 'hello_iambroke';
+
+class LocationHelper {
+  static String generateLocationPreviewImage(
+      {double latitiude, double longitude}) {
+    return null;
+  }
+}
